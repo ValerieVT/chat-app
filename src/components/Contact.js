bc704763b80ef69ee0cb33595b7b597c4617d833
Valerie TV <vtylskivincent@gmail.com>
2020-10-17 19:25:32 +0200

    Bug of online-status fixed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 function Contact(props) {
-    const whichColorStatus = [props.status ? 'status-online': 'status-offline'];
-    const whichTextStatus = [props.status ? 'Online' : 'Offline'];
-    return(
-        <div className='Contact'>
-            <img className='avatar' src={props.avatar} alt='{props.name}' />
-            <div>
-                <p className='name'>{props.name}</p>
-                <span className='status'><span className={whichColorStatus}></span>{whichTextStatus}</span>
-            </div>
-        </div>
-    );
+  const whichColorStatus = props.online ? "status-online" : "status-offline";
+  const whichTextStatus = props.online ? "Online" : "Offline";
+  return (
+    <div className="Contact">
+      <img className="avatar" src={props.avatar} alt="{props.name}" />
+      <div>
+        <p className="name">{props.name}</p>
+        <span className="status">
+          <span className={whichColorStatus}></span>
+          {whichTextStatus}
+        </span>
+      </div>
+    </div>
+  );
 }
 
 Contact.propTypes = {
-    status: PropTypes.bool.isRequired,
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired
-  };
-  
+  status: PropTypes.bool.isRequired,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
+};
+
 export default Contact;
